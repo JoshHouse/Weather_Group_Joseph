ffa231e0d812f0eacac94839f67cee12e926ed46
Joseph <ajaeroj22@students.ecu.edu>
2025-02-25 00:23:51 -0500

    I added the Sidebar and also made sure that the Weathercondition page will call the sidebar on reeact

---
+++ src/Components/Sidebar.js
@@ -0,0 +1,19 @@
+﻿import React from "react";
+import "./Sidebar.css";
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <h2>Weather App</h2>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Forecast</a></li>
+                    <li><a href="#">Compare Statistics</a></li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+export default Sidebar;
